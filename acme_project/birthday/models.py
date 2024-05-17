21d47fb70ef960232daaf5cff887ecaa2edaad32
AldenPC <atemirbekov@stud.etu.ru>
2024-05-17 13:40:10 +0300

    2

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -8,3 +8,12 @@ class Birthday(models.Model):
     )
     birthday = models.DateField('Дата рождения')
     
+# class Contest(models.Model):
+#     title = models.TextField('Название', max_length=20)
+#     description = models.TextField('Описание', max_length=20)
+#     price = models.IntegerField(
+#         'Цена', validators=[MinValueValidator(1), MaxValueValidator(9)], help_text='Рекомендованная розничная цена'
+#     )
+#     comment = models.TextField(
+#         'Комментарий', blank = False
+#     )
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,13 +1,13 @@
 # birthday/views.py
 from django.shortcuts import render
-
+from .models import Birthday
 # Импортируем класс BirthdayForm, чтобы создать экземпляр формы.
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
 
 
 def birthday(request):
-    form = BirthdayForm(request.GET or None)
+    form = BirthdayForm(request.POST or None)
     # Создаём словарь контекста сразу после инициализации формы.
     context = {'form': form}
     # Если форма валидна...
@@ -17,6 +17,14 @@ def birthday(request):
             # ...и передаём в неё дату из словаря cleaned_data.
             form.cleaned_data['birthday']
         )
+        form.save()
         # Обновляем словарь контекста: добавляем в него новый элемент.
         context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context) 
+    return render(request, 'birthday/birthday.html', context)
+
+def birthday_list(request):
+    # Получаем все объекты модели Birthday из БД.
+    birthdays = Birthday.objects.all()
+    # Передаём их в контекст шаблона.
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context) 
